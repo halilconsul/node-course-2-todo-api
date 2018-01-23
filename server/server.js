a960fb564d21413b84bfc428be251bac7577089c
halil <halil_consul@list.ru>
2018-01-23 23:44:17 +0300

    Added GET request

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  // console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
@@ -32,6 +31,18 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({todos});
+  }, e => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started server on port 3000');
 });
+
+module.exports = {
+  app
+};
